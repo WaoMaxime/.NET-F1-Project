9ec1da5d73cd0d02010b8e51d7fc16406cf42db4
Maxime <maxime.blondeel@student.kdg.be>
2024-10-03 11:16:16 +0200

    Small Structure changes

---
+++ UI-CA/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CA;
+
+class Program
+{
+    static void Main(string[] args) // or simply static void Main()
+    {
+        ConsoleUi ui = new ConsoleUi();
+        ui.Run();
+    }
+}
